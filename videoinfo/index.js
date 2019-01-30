0708260f3edd691e9657ccff78df36f2c9c0e67e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-01-29 21:23:27 -0500

    render video duration in html

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -3,14 +3,20 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('video:submit', (event, path) => {
   // receive info into Electron
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log(metadata.format.duration);
+    // send info from electron to main window
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration
+    );
   });
 });
