02d125bf7627da9eb876e4fc2f923f66fbae3da1
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-04-06 10:44:34 -0400

    open converted file in folder

--- convert/index.js
+++ convert/index.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const _ = require('lodash');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
@@ -40,7 +40,12 @@ ipcMain.on('conversion:start', (event, videos) => {
 
     ffmpeg(video.path)
       .output(outputPath)
+      .on('progress', ({ timemark }) => mainWindow.webContents.send('conversion:progress', { video, timemark }))
       .on('end', () => mainWindow.webContents.send('conversion:end', { video, outputPath }))
       .run();
   });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
 });
